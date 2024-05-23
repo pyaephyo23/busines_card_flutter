9420aa0ebf212f841daee6b1cad0812512dce947
Pyae Phyo Kyaw <137527401+pyaephyo23@users.noreply.github.com>
2024-05-23 13:52:50 +0630

    add font and profile

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.teal,
         // appBar: AppBar(
         //   backgroundColor: Colors.black,
         //   title: const Center(
@@ -25,13 +25,79 @@ class MyApp extends StatelessWidget {
         // ),
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              CircleAvatar(
+              const CircleAvatar(
                 radius: 50,
-                backgroundImage:AssetImage('images/profile.png'),
-                backgroundColor: Colors.blueGrey,
+                backgroundImage: AssetImage('images/profile.jpg'),
               ),
-              Text("Pyae Phyo Kyaw")
+              Text(
+                "Pyae Phyo Kyaw",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Source Code Pro',
+                ),
+              ),
+              Text(
+                "Flutter Developer",
+                style: TextStyle(
+                  color: Colors.blueGrey.shade50,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Roboto',
+                  letterSpacing: 5,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal[100],
+                ),
+              ),
+              const Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                    size: 30,
+                  ),
+                  title: Text(
+                    "+95 925 198 5454",
+                    style: TextStyle(
+                      color: Colors.teal,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Roboto',
+                      letterSpacing: 5,
+                    ),
+                  ),
+                ),
+              ),
+              const Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                    size: 30,
+                  ),
+                  title: Text(
+                    "pyaephyo.23k@gmail.com",
+                    style: TextStyle(
+                      color: Colors.teal,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Roboto',
+                    ),
+                  ),
+                )
+              )
             ],
           ),
         ),
